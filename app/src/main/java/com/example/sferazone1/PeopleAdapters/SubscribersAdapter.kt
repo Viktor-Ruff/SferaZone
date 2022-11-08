203a0c8ed728a4f0ae0ff2de164dabc250d26410
Viktor-Ruff <HamiltonE.T.O>
2022-11-09 01:02:18 +0400

    commit in homework4

---
+++ app/src/main/java/com/example/sferazone1/PeopleAdapters/SubscribersAdapter.kt
@@ -0,0 +1,127 @@
+package com.example.sferazone1.PeopleAdapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.sferazone1.databinding.ItemPeopleProfilesImageBinding
+import com.example.sferazone1.model.PeopleModel
+
+/**
+ * Created by Viktor-Ruff
+ * Date: 08.11.2022
+ * Time: 22:52
+ */
+class SubscribersAdapter(clickListener: ClickListener) :
+    RecyclerView.Adapter<SubscribersAdapter.SubscribersViewHolder>(), Filterable {
+
+    private var peopleList: List<PeopleModel> = arrayListOf()
+    private var peopleListFiltered: List<PeopleModel> = arrayListOf()
+    private var clickListener: ClickListener = clickListener
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(people: List<PeopleModel>) {
+        this.peopleList = people
+        this.peopleListFiltered = people
+        notifyDataSetChanged()
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubscribersViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemPeopleProfilesImageBinding.inflate(inflater, parent, false)
+        return SubscribersViewHolder(binding)
+    }
+
+
+    @SuppressLint("NotifyDataSetChanged", "SetTextI18n")
+    override fun onBindViewHolder(holder: SubscribersViewHolder, position: Int) {
+
+        val people = peopleList[position]
+        /*val people = listPeople[position]*/
+        holder.itemView.setOnClickListener() {
+            clickListener.clickedItem(people)
+        }
+
+        with(holder.binding) {
+            tvPeopleProfileName.text = people.name
+            tvPeopleProfileSubscribe.isChecked = people.status
+
+            if (people.status) {
+                tvPeopleProfileSubscribe.text = "Unsubscribe"
+            } else {
+                tvPeopleProfileSubscribe.text = "Subscribe"
+            }
+
+            Glide
+                .with(ivPeopleProfileImage.context)
+                .load(people.profileImage)
+                .circleCrop()
+                .into(ivPeopleProfileImage)
+
+            tvPeopleProfileSubscribe.setOnClickListener() {
+                people.status = !people.status
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+    interface ClickListener {
+        fun clickedItem(people: PeopleModel)
+    }
+
+
+    override fun getItemCount(): Int {
+        return peopleList.size
+    }
+
+
+    class SubscribersViewHolder(
+        val binding: ItemPeopleProfilesImageBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+
+
+    override fun getFilter(): Filter {
+        val filter = object : Filter() {
+
+            override fun performFiltering(p0: CharSequence?): FilterResults {
+
+                var filterResults = FilterResults()
+                var filteredArrayList = ArrayList<PeopleModel>()
+
+                if (p0 == null || p0.isEmpty()) {
+                    /*filteredArrayList.addAll(peopleListFiltered)*/
+                    filterResults.values = peopleListFiltered
+                    filterResults.count = peopleListFiltered.size
+                } else {
+                    var searchChar = p0.toString().toLowerCase()
+
+                    for (people in peopleListFiltered) {
+                        if (people.name.toLowerCase().contains(searchChar)) {
+                            filteredArrayList.add(people)
+                        }
+                    }
+
+                    filterResults.values = peopleListFiltered
+                    filterResults.count = peopleListFiltered.size
+                }
+
+                return filterResults
+            }
+
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+
+
+                peopleList = p1!!.values as List<PeopleModel>
+                notifyDataSetChanged()
+            }
+        }
+        return filter
+    }
+
+}
--- app/src/main/java/com/example/sferazone1/PeopleScreenFragment.kt
+++ app/src/main/java/com/example/sferazone1/PeopleScreenFragment.kt
@@ -1,24 +1,29 @@
 package com.example.sferazone1
 
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.example.sferazone1.PeopleAdapters.SubscribersAdapter
 import com.example.sferazone1.adapters.PeopleAdapter
 import com.example.sferazone1.databinding.FragmentPeopleScreenBinding
+import com.example.sferazone1.model.PeopleModel
 import com.example.sferazone1.model.UserService
 
 
-class PeopleScreenFragment : Fragment() {
+class PeopleScreenFragment : Fragment(), SubscribersAdapter.ClickListener {
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: FragmentPeopleScreenBinding
+    private lateinit var subscribersAdapter:SubscribersAdapter
+    private lateinit var toolbar: Toolbar
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,13 +42,59 @@ class PeopleScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val userService = UserService()
+
         binding = FragmentPeopleScreenBinding.bind(view)
-        binding.peopleRecycler.adapter = PeopleAdapter(userService.initPeopleList())
+
+        initNavigation()
+        initRecycler()
+
+
+       /* binding.peopleRecycler.adapter = PeopleAdapter(userService.initPeopleList())*/
+
+
+
+
+
+    }
+
+    private fun initRecycler(){
+        val userService = UserService()
+        subscribersAdapter = SubscribersAdapter(this)
+        subscribersAdapter.setData(userService.initPeopleList())
+        binding.peopleRecycler.adapter = subscribersAdapter
+
+    }
+
+    private fun initNavigation(){
+        toolbar = binding.toolbarPeople
+
         navController = findNavController()
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        binding.appBarPeople.setupWithNavController(navController, appBarConfiguration)
+        binding.toolbarPeople.setupWithNavController(navController, appBarConfiguration)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_people_app_bar, menu)
+        val item = menu.findItem(R.id.search_action)
+        val searchView = item.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                subscribersAdapter.filter.filter(newText)
+                return true
+            }
+        })
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun clickedItem(people: PeopleModel) {
+        TODO("Not yet implemented")
     }
 
 }
